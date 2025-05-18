0ad3e2a98a067caefd5cc22bd78f07565ed51b8b
MattNardizzi <Nardizzi.matt@gmail.com>
2025-05-18 16:04:58 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true,          // 👈 turns on the App Router
+  },
 };
 
 module.exports = nextConfig;
